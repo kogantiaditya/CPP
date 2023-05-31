63d2a2633c66cf0e01da6b52b019f106b9eddf03
kogantiaditya <aditya.koganti@slu.edu>
2023-05-31 11:41:15 -0500

    A04 - code corrections

--- OpenMP/src/clock_test3.cpp
+++ OpenMP/src/clock_test3.cpp
@@ -14,14 +14,14 @@ int main()
 
     sleep(5);
 
-    cout << "2" < < < endl;
+    cout << "2" << endl;
 
     auto end = chrono::steady_clock::now();
 
     // auto end = chrono::high_resolution_clock::now();
 
     chrono::duration<double> elapsed_seconds = end - begin;
-    cout << "Elapsed Time = " < < < elapsed_seconds.count() << "s" << endl;
+    cout << "Elapsed Time = " << elapsed_seconds.count() << "s" << endl;
 
     return 0;
 }
